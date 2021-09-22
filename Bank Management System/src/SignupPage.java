abc5c73b54a9e60bc3ffe2bf9794fc7add0fc47a
samir571 <sameershrestha68@gmail.com>
2021-09-22 13:12:24 +0545

    Register page updated

--- Bank Management System/src/SignupPage.java
+++ Bank Management System/src/SignupPage.java
@@ -20,46 +20,58 @@ Font fon1,fon2;
 public SignupPage() {
 	frame = new JFrame("User Registration Page");
 	Color color1 = new Color(0,204,210);
+	Font fon = new Font("arial",Font.PLAIN,15);
+
 	frame.getContentPane().setBackground(color1);
+	
+	Icon bg = new ImageIcon(this.getClass().getResource("register.png"));
+	JLabel background = new JLabel(bg);
+	background.setBounds(20, 20, 200, 200); 
+	frame.getContentPane().add(background);
+	
 	fon1 = new Font("arial", Font.BOLD,22);
 	fon2 = new Font("arial",Font.BOLD,19);
 	lbl_heading = new JLabel("User Registration");
 	lbl_heading.setFont(fon1);
-	lbl_heading.setBounds(120,30,200,30);
+	lbl_heading.setForeground(Color.blue);
+	lbl_heading.setBounds(200,20,200,30);
 	
 	lbl_fullname = new JLabel("Fullname:");
 	lbl_fullname.setFont(fon2);
-	lbl_fullname.setBounds(40,80,120,30);
+	lbl_fullname.setBounds(250,80,120,30);
 	frame.add(lbl_fullname);
 	
 	lbl_username = new JLabel("Username:");
 	lbl_username.setFont(fon2);
-	lbl_username.setBounds(40,140,120,30);
+	lbl_username.setBounds(250,180,120,30);
 	frame.add(lbl_username);
 	
 	lbl_password = new JLabel("Password:");
 	lbl_password.setFont(fon2);
-	lbl_password.setBounds(40,200,120,30);
+	lbl_password.setBounds(250,280,120,30);
 	frame.add(lbl_password);
 	
 	txt_fname = new JTextField();
-	txt_fname.setBounds(160,80,120,30);
+	txt_fname.setFont(fon);
+	txt_fname.setBounds(350,80,160,30);
 	frame.add(txt_fname);
 	
 	txt_username = new JTextField();
-	txt_username.setBounds(160,140,120,30);
+	txt_username.setFont(fon);
+	txt_username.setBounds(350,180,160,30);
 	frame.add(txt_username);
 	
 	txt_pw = new JPasswordField();
-	txt_pw.setBounds(160,200,120,30);
+	txt_username.setFont(fon);
+	txt_pw.setBounds(350,280,160,30);
 	frame.add(txt_pw);
 	
 	btn_register = new JButton("Register");
-	btn_register.setBounds(170,250,100,40);
+	btn_register.setBounds(250,370,100,40);
 	btn_register.addActionListener(this);
 	frame.add(btn_register);
 	
-	frame.setSize(400,400);
+	frame.setSize(600,500);
 	frame.add(lbl_heading);
 	frame.setLayout(null);
 	frame.setVisible(true);
